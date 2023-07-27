d0786d78fe3340957109e376c87cd790af4be36d
Preisler <preisler.andras@gmail.com>
2023-07-27 22:44:21 +0200

    init project

---
+++ src/main/java/PetrikDev/ProjectTree/ProjectTreeApplication.java
@@ -0,0 +1,13 @@
+package PetrikDev.ProjectTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PetrikDev/ProjectTree/ProjectTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package PetrikDev.ProjectTree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
